91ffd09357d0b79ed2fecbd4e582a096815db27c
Karen Ical <79215104+Keibwi@users.noreply.github.com>
2022-07-14 23:56:37 -0600

    Create user.js

---
+++ js/user.js
@@ -0,0 +1,29 @@
+export let user_ob = [
+    {
+        id:100,
+        nikname:'anna0',
+        password:'white00',
+        name:'Anna Adison',
+        adress: '1325 Candy Rd, San Francisco, CA 96123',
+        email: 'anna.addison@yahoo.com',
+        phone: 3841019535,
+    },
+    {
+        id:101,
+        nikname:'carol1',
+        password:'orange01',
+        name:'Carol Campbell',
+        adress: '1931 Brown St, Gainesville, FL 85321',
+        email: 'carol.campbell@aol.com',
+        phone: 2548952651,
+    },
+    {
+        id:101,
+        nikname:'carol1',
+        password:'orange01',
+        name:'Carol Campbell',
+        adress: '1931 Brown St, Gainesville, FL 85321',
+        email: 'carol.campbell@aol.com',
+        phone: 2548952651,
+    },
+]
